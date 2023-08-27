ffc89cec247c18ac76fae30b1a7f978ee04379df
Dmitry Ilichov <dmitryilichov@gmail.com>
2023-08-27 15:30:59 +0300

    completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const config = [];
+    let i = 0;
+    let counter = 0;
+    
+    bracketsConfig.forEach(function() {
+        config.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+        i++;
+    });
+    console.log(config)
+    
+   while (config.length > counter) {
+        if (str.includes(config[counter])) {
+            str = str.replace(config[counter], '');
+            counter = 0;
+        } else {
+            counter++;
+    }
+}
+    return str === '';
 }
